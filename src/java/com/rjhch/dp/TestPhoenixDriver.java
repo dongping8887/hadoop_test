68ba07185b759dbcd6e2e03c67b2cf4ec4a2e07e
jyblims <572181366@qq.com>
2018-10-26 22:28:43 +0800

    1.实现hbase的多租户功能

--- src/java/com/rjhch/dp/TestPhoenixDriver.java
+++ src/java/com/rjhch/dp/TestPhoenixDriver.java
@@ -1,24 +1,30 @@
 package com.rjhch.dp;
 
+import org.apache.hadoop.security.UserGroupInformation;
+
 import java.sql.*;
+import java.util.Properties;
 
 public class TestPhoenixDriver {
 
     public static void main(String[] args) throws Exception {
         Statement stmt = null;
         ResultSet rs = null;
-
+        UserGroupInformation.loginUserFromKeytab("hadoop/hadoop01@HADOOP.COM","D:\\keytab\\hadoop.keytab");
         Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
-        Connection conn = DriverManager.getConnection("jdbc:phoenix:hadoop01,hadoop02,hadoop03:2181");
+
+        Properties props = new Properties();
+        props.setProperty("TenantId", "lisi");
+        Connection conn = DriverManager.getConnection("jdbc:phoenix:hadoop01,hadoop02,hadoop03:2181", props);
 
         /* 在Phoenix中，如果table name/view name、column name等字符串不加上双引号就会被认为是大写。所以，这里的brand_name要加上双引号  */
-        PreparedStatement pstmt = conn.prepareStatement("select * from EV_RPY_TRANS_FLOW;");
+        PreparedStatement pstmt = conn.prepareStatement("select * from EV_RPY_TRANS_FLOW");
         rs = pstmt.executeQuery();
 
         StringBuffer sb = new StringBuffer();
 
         while (rs.next()) {
-            System.err.println(rs.getString("pk")+"---"+rs.getString("BATCH_DATE")+"---"+rs.getString("ACCEPT_ORG_CODE"));
+            System.err.println(rs.getString("pk") + "---" + rs.getString("BATCH_DATE") + "---" + rs.getString("ACCEPT_ORG_CODE"));
             //System.err.println("\n=========================================================");
         }
         /* 关闭资源*/
